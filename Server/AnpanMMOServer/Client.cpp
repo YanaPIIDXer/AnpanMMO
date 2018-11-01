fd086bf0d0be42c5d7caccf52299b7736ce75222
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-01 17:49:55 +0900

    受信処理の作成。

--- Server/AnpanMMOServer/Client.cpp
+++ Server/AnpanMMOServer/Client.cpp
@@ -6,4 +6,25 @@ Client::Client(const shared_ptr<tcp::socket> &pInSocket)
 	: pSocket(pInSocket)
 	, bIsConnected(true)
 {
+	AsyncRecv(&RecvBuffer[0], 0);
+}
+
+
+// 受信開始.
+void Client::AsyncRecv(u8 *pBuffer, int Offset)
+{
+	pSocket->async_receive(asio::buffer(pBuffer, 1024 - Offset),
+		bind(&Client::OnRecv, this, asio::placeholders::error, asio::placeholders::bytes_transferred));
+}
+
+// 受信した。
+void Client::OnRecv(const system::error_code &ErrorCode, size_t Size)
+{
+	if (ErrorCode)
+	{
+		bIsConnected = false;
+		return;
+	}
+
+	AsyncRecv(&RecvBuffer[0], 0);
 }
--- Server/AnpanMMOServer/Client.h
+++ Server/AnpanMMOServer/Client.h
@@ -26,6 +26,16 @@ private:
 	// 接続されているか？
 	bool bIsConnected;
 
+	// 受信バッファ.
+	array<u8, 1024> RecvBuffer;
+
+
+	// 受信開始.
+	void AsyncRecv(u8 *pBuffer, int Offset);
+
+	// 受信した。
+	void OnRecv(const system::error_code &Error, size_t Size);
+
 };
 
 #endif		// #ifndef __CLIENT_H__
