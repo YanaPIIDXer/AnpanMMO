dd8ba9867438e3ea8912039638145f7bb7355843
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-01 20:06:23 +0900

    ログインリクエスト受信後の処理として、MySQLへのアクセス処理が出来るまではダミーを返すようにした。

--- Server/AnpanMMOServer/ClientState/ClientStateTitle.cpp
+++ Server/AnpanMMOServer/ClientState/ClientStateTitle.cpp
@@ -2,6 +2,8 @@
 #include "ClientStateTitle.h"
 #include "MemoryStream/MemoryStreamInterface.h"
 #include "Packet/PacketLogInRequest.h"
+#include "Packet/PacketLogInResult.h"
+#include "Client.h"
 
 // コンストラクタ
 ClientStateTitle::ClientStateTitle(Client *pInParent)
@@ -29,4 +31,7 @@ void ClientStateTitle::OnRecvLogInRequest(MemoryStreamInterface *pStream)
 	PacketLogInRequest Packet;
 	Packet.Serialize(pStream);
 
+	// とりあえずダミーデータを返す。
+	PacketLogInResult ResultPacket(PacketLogInResult::Success, 1);
+	GetParent()->SendPacket(&ResultPacket);
 }
