482d9335d2812575b81cd8f89afe37706d996cdc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-01 20:15:54 +0900

    GameInstance追加。

---
+++ Client/AnpanMMO/Source/AnpanMMO/MMOGameInstance.cpp
@@ -0,0 +1,7 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#include "MMOGameInstance.h"
+
+
+
+
---
+++ Client/AnpanMMO/Source/AnpanMMO/MMOGameInstance.h
@@ -0,0 +1,21 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MMOGameInstance.generated.h"
+
+/**
+ * GameInstance
+ */
+UCLASS()
+class ANPANMMO_API UMMOGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+private:
+	
+};
