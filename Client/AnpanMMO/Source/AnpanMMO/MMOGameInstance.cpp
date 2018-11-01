d22a6b6b99b366878a4c7167eaacd2b2d6c51d35
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-01 21:35:07 +0900

    ゲームサーバへの接続周りの各種関数実装。

--- Client/AnpanMMO/Source/AnpanMMO/MMOGameInstance.cpp
+++ Client/AnpanMMO/Source/AnpanMMO/MMOGameInstance.cpp
@@ -2,6 +2,38 @@
 
 #include "MMOGameInstance.h"
 
+// コンストラクタ
+UMMOGameInstance::UMMOGameInstance(const FObjectInitializer &ObjectInitializer)
+	: Super(ObjectInitializer)
+	, pConnection(nullptr)
+{
+}
 
+// 終了時の処理.
+void UMMOGameInstance::Shutdown()
+{
+	Super::Shutdown();
+	
+	Close();
+}
 
+// 接続.
+bool UMMOGameInstance::Connect(const FString &Host, int Port)
+{
+	pConnection = new GameServerConnection();
+	if (!pConnection->Connect(Host, Port))
+	{
+		Close();
+		return false;
+	}
+	return true;
+}
 
+// 切断.
+void UMMOGameInstance::Close()
+{
+	if (pConnection == nullptr) { return; }
+
+	delete pConnection;
+	pConnection = nullptr;
+}
--- Client/AnpanMMO/Source/AnpanMMO/MMOGameInstance.h
+++ Client/AnpanMMO/Source/AnpanMMO/MMOGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Connection/GameServerConnection.h"
 #include "MMOGameInstance.generated.h"
 
 /**
@@ -16,6 +17,27 @@ class ANPANMMO_API UMMOGameInstance : public UGameInstance
 	
 public:
 
+	// コンストラクタ
+	UMMOGameInstance(const FObjectInitializer &ObjectInitializer);
+
+	// デストラクタ
+	virtual ~UMMOGameInstance() {}
+
+	// 終了時の処理.
+	virtual void Shutdown() override;
+
+	// 接続.
+	bool Connect(const FString &Host, int Port);
+
+	// 切断.
+	void Close();
+
+	// 接続されているか？
+	bool IsConnected() const { return (pConnection != nullptr && pConnection->IsConnected()); }
+
 private:
+
+	// ゲームサーバへの接続.
+	GameServerConnection *pConnection;
 	
 };
