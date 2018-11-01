ad6191510c8edfac3cf3061cc22afa3d8f3db065
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-01 14:53:39 +0900

    定数定義クラスを追加。

---
+++ Server/AnpanMMOServer/Config.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+#include "Config.h"
+
+const int Config::Port = 4424;
---
+++ Server/AnpanMMOServer/Config.h
@@ -0,0 +1,17 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/**
+ * 定数定義.
+ */
+class Config
+{
+
+public:
+
+	// ポート番号.
+	static const int Port;
+
+};
+
+#endif		// #ifndef __CONFIG_H__
