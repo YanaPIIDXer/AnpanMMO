738c3f72d93d6537df7f0089d50c4f84503d8a0f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-02 13:33:02 +0900

    定数定義クラスを追加。 サーバホストとポートを定義。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Config.h
@@ -0,0 +1,21 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 定数定義.
+ */
+class ANPANMMO_API Config
+{
+
+public:
+
+	// サーバホスト
+	static const FString ServerHost;
+
+	// サーバポート
+	static const int ServerPort;
+
+};
