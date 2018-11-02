bae520c9bfd032243fdea5fcaf829143aef2f009
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-02 17:17:09 +0900

    クエリ実行クラス、バインドクラス追加。 ※クエリ実行クラスはまだ途中。一旦コミット。

---
+++ Server/AnpanMMOServer/MySQL/MySqlBinds.h
@@ -0,0 +1,49 @@
+#ifndef __MYSQLBINDS_H__
+#define __MYSQLBINDS_H__
+
+#ifdef _WIN32
+#include <mysql.h>
+#else
+#include <mysql/mysql.h>
+#endif
+
+#include <vector>
+
+/**
+ * バインド
+ */
+class MySqlBinds
+{
+
+public:
+
+	// コンストラクタ
+	MySqlBinds();
+
+	// デストラクタ
+	~MySqlBinds();
+
+	// int追加.
+	void AddInt(int *pPtr);
+
+	// 文字列追加.
+	void AddString(char *pPtr);
+
+	// 取得.
+	MYSQL_BIND *Get() { return pBinds; }
+
+private:
+
+	// バインド
+	MYSQL_BIND *pBinds;
+
+	// 配列長.
+	int Length;
+
+	// 領域拡張.
+	void Reallocate();
+
+};
+
+#endif		// #ifndef __MYSQLBINDS_H__
+
