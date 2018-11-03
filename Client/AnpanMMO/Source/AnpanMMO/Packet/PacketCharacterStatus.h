7eaa3bf886dedba464e8afa83e2781a7bcad3dae
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-03 13:27:14 +0900

    キャラクタステータスパケット追加。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Packet/PacketCharacterStatus.h
@@ -0,0 +1,49 @@
+#ifndef __PACKETCHARACTERSTATUS_H__
+#define __PACKETCHARACTERSTATUS_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketCharacterStatus  : public PacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return CharacterStatus; }
+
+	
+
+	s32 Hp;
+	s32 MaxHp;
+	s32 Atk;
+	s32 Def;
+	s32 Exp;
+	
+
+	PacketCharacterStatus()
+	{
+	}
+
+	PacketCharacterStatus(s32 InHp, s32 InMaxHp, s32 InAtk, s32 InDef, s32 InExp)
+	{
+		Hp = InHp;
+		MaxHp = InMaxHp;
+		Atk = InAtk;
+		Def = InDef;
+		Exp = InExp;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&Hp);
+		pStream->Serialize(&MaxHp);
+		pStream->Serialize(&Atk);
+		pStream->Serialize(&Def);
+		pStream->Serialize(&Exp);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETCHARACTERSTATUS_H__
