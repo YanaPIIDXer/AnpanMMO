760b96282a206f06f6f3ba5e930990d9c4e1d918
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-03 12:00:10 +0900

    キャラクタ基底クラスを追加。

---
+++ Server/AnpanMMOServer/Character/CharacterBase.h
@@ -0,0 +1,18 @@
+#ifndef __CHARACTERBASE_H__
+#define __CHARACTERBASE_H__
+
+/**
+ * キャラクタ基底クラス
+ */
+class CharacterBase
+{
+
+	// コンストラクタ
+	CharacterBase();
+
+	// デストラクタ
+	virtual ~CharacterBase() {}
+
+};
+
+#endif		// #ifndef __CHARACTERBASE_H__
