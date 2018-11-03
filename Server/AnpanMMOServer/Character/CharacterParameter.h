d657a5a5aa5cb225c225e57e8b05a16d75aba726
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-03 13:08:37 +0900

    キャラクタパラメータ構造体を追加。

---
+++ Server/AnpanMMOServer/Character/CharacterParameter.h
@@ -0,0 +1,24 @@
+#ifndef __CHARACTERPARAMETER_H__
+#define __CHARACTERPARAMETER_H__
+
+/**
+ * キャラクタパラメータ
+ */
+struct CharacterParameter
+{
+
+	// HP
+	int Hp;
+
+	// 最大HP
+	int MaxHp;
+
+	// 攻撃力.
+	int Atk;
+
+	// 防御力.
+	int Def;
+
+};
+
+#endif		// #ifndef __CHARACTERPARAMETER_H__
