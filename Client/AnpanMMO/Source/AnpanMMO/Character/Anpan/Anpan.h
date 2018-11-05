262947dd0cf76e74f35b92671cbdebc75c761aaf
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-05 16:06:39 +0900

    アンパンクラスを追加。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Character/Anpan/Anpan.h
@@ -0,0 +1,46 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Anpan.generated.h"
+
+class AnpanData;
+
+/**
+ * アンパンクラス
+ */
+UCLASS()
+class ANPANMMO_API AAnpan : public AActor
+{
+
+	GENERATED_BODY()
+	
+public:	
+
+	// 生成.
+	static AAnpan *Spawn(UWorld *pWorld, const AnpanData &Data);
+
+	// コンストラクタ
+	AAnpan(const FObjectInitializer &ObjectInitializer);
+
+	// デストラクタ
+	virtual ~AAnpan() {}
+
+private:
+
+	// メッシュのパス
+	static const TCHAR *MeshPath;
+
+	// メッシュComponent
+	UPROPERTY()
+	UStaticMeshComponent *pMeshComponent;
+
+	// HP
+	int Hp;
+
+	// 最大HP
+	int MaxHp;
+	
+};
