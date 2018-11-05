d8096d18769003fb03665f0a75a5ef0ea885fb12
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-06 06:45:47 +0900

    パラメータを増加させるようにした。

--- Server/AnpanMMOServer/Character/PlayerCharacter.cpp
+++ Server/AnpanMMOServer/Character/PlayerCharacter.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "PlayerCharacter.h"
+#include "Math/Random.h"
 
 // コンストラクタ
 PlayerCharacter::PlayerCharacter(Client *pInClient, int MaxHp, int Atk, int Def, int InExp)
@@ -14,5 +15,8 @@ PlayerCharacter::PlayerCharacter(Client *pInClient, int MaxHp, int Atk, int Def,
 // レベルアップコールバック
 void PlayerCharacter::OnLevelUp()
 {
-
+	int MaxHp = Random::Range<int>(10, 20);
+	int Atk = Random::Range<int>(10, 20);
+	int Def = Random::Range<int>(10, 20);
+	AddParameter(MaxHp, Atk, Def);
 }
