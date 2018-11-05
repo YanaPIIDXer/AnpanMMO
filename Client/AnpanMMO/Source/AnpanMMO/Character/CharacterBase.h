c8fa7b19a140a3c65f6d230f0dc23a398f3bbe4a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-05 21:13:48 +0900

    HP周りの実装。

--- Client/AnpanMMO/Source/AnpanMMO/Character/CharacterBase.h
+++ Client/AnpanMMO/Source/AnpanMMO/Character/CharacterBase.h
@@ -22,8 +22,36 @@ public:
 	// デストラクタ
 	virtual ~ACharacterBase() {}
 
+	// ダメージを与える。
+	void ApplyDamage(int32 Value);
+
+	// 死んでる？
+	bool IsDead() const { return (Hp <= 0); }
+
+	// HP取得
+	UFUNCTION(BlueprintPure, Category = "Status")
+	int32 GetHp() const { return Hp; }
+
+	// 最大HP取得.
+	UFUNCTION(BlueprintPure, Category = "Status")
+	int32 GetMaxHp() const { return MaxHp; }
+
 protected:
 
+	// 初期化.
+	void Initialize(int32 InHp, int32 InMaxHp);
+
+	// 死亡イベント
+	UFUNCTION(BlueprintNativeEvent, Category = "Character")
+	void OnDead();
+	void OnDead_Implementation() {}
+
 private:
+
+	// HP
+	int32 Hp;
+
+	// 最大HP
+	int32 MaxHp;
 	
 };
