b2fd89a196c1e6736b41ded113303f15dfae1cdd
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-06 07:05:02 +0900

    経験値増加パケットを追加。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Packet/PacketAddExp.h
@@ -0,0 +1,37 @@
+#ifndef __PACKETADDEXP_H__
+#define __PACKETADDEXP_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketAddExp  : public PacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return AddExp; }
+
+	
+
+	s32 Exp;
+	
+
+	PacketAddExp()
+	{
+	}
+
+	PacketAddExp(s32 InExp)
+	{
+		Exp = InExp;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&Exp);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETADDEXP_H__
--- Server/AnpanMMOServer/Packet/PacketID.h
+++ Server/AnpanMMOServer/Packet/PacketID.h
@@ -8,6 +8,7 @@ enum PacketID
 	Attack,
 	Damage,
 	GameReady,
+	AddExp,
 	LevelUp,
 	LogInRequest,
 	LogInResult,
