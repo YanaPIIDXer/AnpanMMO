bf9fbb17367bbc6effca8c9cc866f3bae145ede6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-07 14:50:56 +0900

    プレイヤー管理クラスを追加。

---
+++ Server/AnpanMMOServer/PlayerManager.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "PlayerManager.h"
+
+// コンストラクタ
+PlayerManager::PlayerManager()
+{
+}
+
+// 毎フレームの処理.
+void PlayerManager::Poll()
+{
+	PlayerMap::iterator It = PlayerList.begin();
+	while (It != PlayerList.end())
+	{
+		if (It->second.expired())
+		{
+			It = PlayerList.erase(It);
+		}
+		else
+		{
+			++It;
+		}
+	}
+}
+
+// 追加.
+void PlayerManager::Add(u8 Uuid, PlayerCharacterPtr pPlayer)
+{
+	PlayerList[Uuid] = pPlayer;
+}
+
+// 取得.
+PlayerCharacterPtr PlayerManager::Get(u8 Uuid) const
+{
+	PlayerMap::const_iterator It = PlayerList.find(Uuid);
+	if (It == PlayerList.end()) { return PlayerCharacterPtr(); }
+	return It->second;
+}
