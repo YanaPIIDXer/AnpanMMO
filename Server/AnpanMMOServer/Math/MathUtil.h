28b9cba0daadeab4e60d411e2712ba7cc59a9c3b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-08 15:29:00 +0900

    演算関係クラスを追加。 補間処理を実装。

---
+++ Server/AnpanMMOServer/Math/MathUtil.h
@@ -0,0 +1,14 @@
+#ifndef __MATHUTIL_H__
+#define __MATHUTIL_H__
+
+// ‰‰ŽZŠÖŒW.
+class MathUtil
+{
+
+	// •âŠÔ.
+	template<class T>
+	T Lerp(T Start, T End, float Rate);
+
+};
+
+#endif		// #ifndef __MATHUTIL_H__
