1b04bbd0d7dd56e8322b044e073a7e2ba0d73162
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-10 22:34:54 +0900

    他人のＨＰ、最大ＨＰを通知するようにした。

--- Client/AnpanMMO/Source/AnpanMMO/Packet/PacketSpawnPlayer.h
+++ Client/AnpanMMO/Source/AnpanMMO/Packet/PacketSpawnPlayer.h
@@ -14,21 +14,27 @@ public:
 	
 
 	u32 Uuid;
+	s32 Hp;
+	s32 MaxHp;
 	
 
 	PacketSpawnPlayer()
 	{
 	}
 
-	PacketSpawnPlayer(u32 InUuid)
+	PacketSpawnPlayer(u32 InUuid, s32 InHp, s32 InMaxHp)
 	{
 		Uuid = InUuid;
+		Hp = InHp;
+		MaxHp = InMaxHp;
 		
 	}
 
 	bool Serialize(MemoryStreamInterface *pStream)
 	{
 		pStream->Serialize(&Uuid);
+		pStream->Serialize(&Hp);
+		pStream->Serialize(&MaxHp);
 		
 		return true;
 	}
