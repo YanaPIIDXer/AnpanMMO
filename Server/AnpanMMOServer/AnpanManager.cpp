20db66274e482b9bdefd7a601379dad568238741
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-11 00:43:43 +0900

    問題の箇所が特定できたので元に戻す。

--- Server/AnpanMMOServer/AnpanManager.cpp
+++ Server/AnpanMMOServer/AnpanManager.cpp
@@ -4,7 +4,7 @@
 #include "Packet/PacketAnpanList.h"
 
 const int AnpanManager::SpawnInterval = 5000;
-const int AnpanManager::AnpanMax = 1;
+const int AnpanManager::AnpanMax = 100;
 
 // コンストラクタ
 AnpanManager::AnpanManager()
