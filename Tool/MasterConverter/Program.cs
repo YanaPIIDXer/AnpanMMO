6945f17cc6709ff6d8316051b20bebd172bd5482
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-11 11:38:51 +0900

    MasterDataディレクトリ内のファイルをForEachで回すようにした。

--- Tool/MasterConverter/Program.cs
+++ Tool/MasterConverter/Program.cs
@@ -3,27 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MasterConverter
 {
 	class Program
 	{
+
+		/// <summary>
+		/// エクセルファイルを格納するパス
+		/// </summary>
+		private static readonly string ExcelFilePath = "MasterData";
+
 		static void Main(string[] args)
 		{
-			ExcelParser Parser = new ExcelParser("MasterData\\Test.xlsx");
-			if(!Parser.Load())
+			string[] Files = Directory.GetFiles(ExcelFilePath);
+			foreach(var FilePath in Files)
 			{
-				Console.WriteLine("Excelファイルの解析に失敗しました。");
-				Console.ReadKey();
-			}
+				ExcelParser Parser = new ExcelParser(FilePath);
+				if (!Parser.Load())
+				{
+					Console.WriteLine("Excelファイルの解析に失敗しました。");
+					Console.ReadKey();
+				}
 
-			SQLGenerator SQLGen = new SQLGenerator("SQLs\\Test.sql", Parser.Columns);
-			if(!SQLGen.Generate())
-			{
-				Console.WriteLine("SQLファイルの生成に失敗しました。");
-				Console.ReadKey();
-			}
+				SQLGenerator SQLGen = new SQLGenerator("SQLs\\Test.sql", Parser.Columns);
+				if (!SQLGen.Generate())
+				{
+					Console.WriteLine("SQLファイルの生成に失敗しました。");
+					Console.ReadKey();
+				}
 
+			}
 		}
 	}
 }
