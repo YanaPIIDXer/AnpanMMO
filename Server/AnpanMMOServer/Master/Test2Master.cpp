9090e27a2f34e0e46cb00361ae2c71afcb127214
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-13 19:44:45 +0900

    文字コード変更。

--- Server/AnpanMMOServer/Master/Test2Master.cpp
+++ Server/AnpanMMOServer/Master/Test2Master.cpp
@@ -1,4 +1,4 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 #include "Test2Master.h"
 #include "MySQL/MySqlConnection.h"
 
--- Server/AnpanMMOServer/Master/TestMaster.cpp
+++ Server/AnpanMMOServer/Master/TestMaster.cpp
@@ -1,4 +1,4 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 #include "TestMaster.h"
 #include "MySQL/MySqlConnection.h"
 
