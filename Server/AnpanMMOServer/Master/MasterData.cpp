4d160f4e0a82632c25f2936e4754e0cc989ec956
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-13 20:09:00 +0900

    Test2Masterも読み込むようにしてみた。

--- Server/AnpanMMOServer/Master/MasterData.cpp
+++ Server/AnpanMMOServer/Master/MasterData.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include "MySQL/MySqlConnection.h"
 #include "TestMaster.h"
+#include "Test2Master.h"
 
 const char *MasterData::DBHost = "127.0.0.1";
 const char *MasterData::DBName = "AnpanMMOMaster";
@@ -37,10 +38,20 @@ bool MasterData::Read()
 	if (!Test.Load(Connection)) { return false; }
 	
 	std::vector<TestItem> TestItems = Test.GetAll();
-	std::cout << "DataCount:" << TestItems.size() << std::endl;
+	std::cout << "TestMaster DataCount:" << TestItems.size() << std::endl;
 	for (std::vector<TestItem>::iterator It = TestItems.begin(); It != TestItems.end(); ++It)
 	{
 		std::cout << "ID:" << It->ID << " Name:" << It->Name << " Hp:" << It->Hp << " Mp:" << It->Mp << " Exp:" << It->Exp << std::endl;
+	}
+
+	Test2Master Test2;
+	if (!Test2.Load(Connection)) { return false; }
+
+	std::vector<Test2Item> Test2Items = Test2.GetAll();
+	std::cout << std::endl << "Test2Master DataCount:" << Test2Items.size() << std::endl;
+	for (std::vector<Test2Item>::iterator It = Test2Items.begin(); It != Test2Items.end(); ++It)
+	{
+		std::cout << "ID:" << It->ID << " Hp:" << It->Hp << " Mp:" << It->Mp << " Exp:" << It->Exp << std::endl;
 	}
 
 	return true;
