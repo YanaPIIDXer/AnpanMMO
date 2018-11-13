553201b8be46a15c1a3c83805f26c0881359ac06
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-13 20:39:23 +0900

    テスト用マスタを削除。

--- Server/AnpanMMOServer/Master/MasterData.cpp
+++ Server/AnpanMMOServer/Master/MasterData.cpp
@@ -2,8 +2,6 @@
 #include "MasterData.h"
 #include <fstream>
 #include "MySQL/MySqlConnection.h"
-#include "Test2Master.h"
-#include "Test3Master.h"
 
 const char *MasterData::DBHost = "127.0.0.1";
 const char *MasterData::DBName = "AnpanMMOMaster";
@@ -33,27 +31,5 @@ bool MasterData::Read()
 		return false;
 	}
 	
-	// テスト２
-	Test2Master Test2;
-	if (!Test2.Load(Connection)) { return false; }
-
-	std::vector<Test2Item> Test2Items = Test2.GetAll();
-	std::cout << "Test2Master DataCount:" << Test2Items.size() << std::endl;
-	for (std::vector<Test2Item>::iterator It = Test2Items.begin(); It != Test2Items.end(); ++It)
-	{
-		std::cout << "ID:" << It->ID << " Hp:" << It->Hp << " Mp:" << It->Mp << " Exp:" << It->Exp << std::endl;
-	}
-
-	// テスト３
-	Test3Master Test3;
-	if (!Test3.Load(Connection)) { return false; }
-
-	std::vector<Test3Item> Test3Items = Test3.GetAll();
-	std::cout << std::endl << "Test3Master DataCount:" << Test2Items.size() << std::endl;
-	for (std::vector<Test3Item>::iterator It = Test3Items.begin(); It != Test3Items.end(); ++It)
-	{
-		std::cout << "ID:" << It->ID << " Hp:" << It->Hp << " Mp:" << It->Mp << " Exp:" << It->Exp << std::endl;
-	}
-
 	return true;
 }
