b559b7ea1a7702feccb847c2802a6805c120e858
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-17 05:12:09 +0900

    PopAreaマスタに移動したカラムを削除。

--- Client/AnpanMMO/Source/AnpanMMO/Master/AreaMaster.h
+++ Client/AnpanMMO/Source/AnpanMMO/Master/AreaMaster.h
@@ -11,14 +11,6 @@ public:
 	u32 ID;
 	std::string Name;
 	u32 AnpanMax;
-	s32 AnpanMinHp;
-	s32 AnpanMaxHp;
-	s32 AnpanMinAtk;
-	s32 AnpanMaxAtk;
-	s32 AnpanMinDef;
-	s32 AnpanMaxDef;
-	s32 AnpanMinExp;
-	s32 AnpanMaxExp;
 
 
 	bool Serialize(MemoryStreamInterface *pStream)
@@ -26,14 +18,6 @@ public:
 		if(!pStream->Serialize(&ID)) { return false; }
 		if(!pStream->Serialize(&Name)) { return false; }
 		if(!pStream->Serialize(&AnpanMax)) { return false; }
-		if(!pStream->Serialize(&AnpanMinHp)) { return false; }
-		if(!pStream->Serialize(&AnpanMaxHp)) { return false; }
-		if(!pStream->Serialize(&AnpanMinAtk)) { return false; }
-		if(!pStream->Serialize(&AnpanMaxAtk)) { return false; }
-		if(!pStream->Serialize(&AnpanMinDef)) { return false; }
-		if(!pStream->Serialize(&AnpanMaxDef)) { return false; }
-		if(!pStream->Serialize(&AnpanMinExp)) { return false; }
-		if(!pStream->Serialize(&AnpanMaxExp)) { return false; }
 
 		return true;
 	}
