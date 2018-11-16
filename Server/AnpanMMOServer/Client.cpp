b5790a656097ea87ef6ae4a8846e1575c7cbfaa3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-17 07:33:22 +0900

    TCP接続を行うクラス TCPConnectionを作成。 Clientをその派生クラスにした。

--- Server/AnpanMMOServer/Client.cpp
+++ Server/AnpanMMOServer/Client.cpp
@@ -11,13 +11,12 @@
 
 // コンストラクタ
 Client::Client(const shared_ptr<tcp::socket> &pInSocket)
-	: pSocket(pInSocket)
-	, bIsConnected(true)
+	: TCPConnection(pInSocket)
 	, pState(new ClientStateTitle(this))
 	, Uuid(0)
 	, CustomerId(0)
 {
-	AsyncRecv(&RecvData[0], 0);
+	AsyncRecv();
 }
 
 // デストラクタ
@@ -61,24 +60,9 @@ void Client::CreateCharacter(int MaxHp, int Atk, int Def, int Exp)
 }
 
 
-// 受信開始.
-void Client::AsyncRecv(u8 *pBuffer, int Offset)
+// データを受信した。
+void Client::OnRecvData(size_t Size)
 {
-	pSocket->async_receive(asio::buffer(pBuffer, RecvDataSize - Offset),
-		bind(&Client::OnRecv, this, asio::placeholders::error, asio::placeholders::bytes_transferred));
-}
-
-// 受信した。
-void Client::OnRecv(const boost::system::error_code &ErrorCode, size_t Size)
-{
-	if (ErrorCode)
-	{
-		bIsConnected = false;
-		return;
-	}
-
-	RecvBuffer.Push(&RecvData[0], Size);
-
 	u8 *pRecvData = RecvBuffer.GetTop();
 	MemoryStreamReader ReadStream(pRecvData, Size);
 	PacketHeader Header;
@@ -88,30 +72,7 @@ void Client::OnRecv(const boost::system::error_code &ErrorCode, size_t Size)
 
 		MemoryStreamReader BodyStream(RecvBuffer.GetTop(), Header.GetPacketSize());
 		pState->AnalyzePacket(Header.GetPacketId(), &BodyStream);
-		
+
 		RecvBuffer.Pop(Header.GetPacketSize());
-	}
-
-	AsyncRecv(&RecvData[0], 0);
-}
-
-// 送信.
-void Client::AsyncSend(const u8 *pBuffer, int Size)
-{
-	shared_ptr<asio::streambuf> pSendBuffer = shared_ptr<asio::streambuf>(new asio::streambuf());
-	pSendBuffer->sputn((const char * ) pBuffer, Size);
-
-	tcp::socket *pSock = pSocket.get();
-	asio::async_write(*pSock, *pSendBuffer,
-		bind(&Client::OnSend, this, asio::placeholders::error, asio::placeholders::bytes_transferred, pSendBuffer));
-}
-
-// 送信した。
-void Client::OnSend(const boost::system::error_code &ErrorCode, size_t Size, shared_ptr<asio::streambuf> SendBuffer)
-{
-	if (ErrorCode)
-	{
-		bIsConnected = false;
-		return;
 	}
 }
