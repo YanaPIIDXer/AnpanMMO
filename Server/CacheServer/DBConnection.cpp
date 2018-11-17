033d4e5f542715404ac1e89b0057509353a8e413
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-17 13:56:01 +0900

    ゲームサーバ側のDBConnectionクラスを移植。 ※後程ゲームサーバ側からこのクラスは消え去る。

---
+++ Server/CacheServer/DBConnection.cpp
@@ -0,0 +1,127 @@
+#include "stdafx.h"
+#include "DBConnection.h"
+#include <fstream>
+
+const std::string DBConnection::UserDataFileName = "DBUserData.txt";
+const char *DBConnection::DBHost = "127.0.0.1";
+const char *DBConnection::DBName = "AnpanMMO";
+DBConnection DBConnection::Instance;
+
+// 開く
+bool DBConnection::Open()
+{
+	std::ifstream FileStream(UserDataFileName.c_str());
+	if (!FileStream)
+	{
+		std::cout << UserDataFileName << " Open Failed..." << std::endl;
+		return false;
+	}
+
+	char UserName[256];
+	char PassWord[256];
+	FileStream.getline(UserName, 256);
+	FileStream.getline(PassWord, 256);
+	FileStream.close();
+
+	return Connection.Connect(DBHost, UserName, PassWord, DBName);
+}
+
+// ユーザデータ読み込み
+bool DBConnection::LoadUserData(char *pUserCode, int &OutId)
+{
+	MySqlQuery Query = Connection.CreateQuery("select Id from UserData where UserCode = ?;");
+	Query.BindString(pUserCode);
+
+	int Id = 0;
+	Query.BindResultInt(&Id);
+	if (!Query.ExecuteQuery()) { return false; }
+
+	if (!Query.Fetch())
+	{
+		// 登録して検索し直す。
+		if (!RegisterUserData(pUserCode)) { return false; }
+		return LoadUserData(pUserCode, OutId);
+	}
+	
+	OutId = Id;
+	return true;
+}
+
+// キャラクタパラメータ読み込み
+bool DBConnection::LoadCharacterParameter(int Id, int &OutMaxHp, int &OutAtk, int &OutDef, int &OutExp)
+{
+	MySqlQuery Query = Connection.CreateQuery("select MaxHp, Atk, Def, Exp from CharacterData where CustomerId = ?");
+	Query.BindInt(&Id);
+
+	Query.BindResultInt(&OutMaxHp);
+	Query.BindResultInt(&OutAtk);
+	Query.BindResultInt(&OutDef);
+	Query.BindResultInt(&OutExp);
+
+	if (!Query.ExecuteQuery()) { return false; }
+	if (!Query.Fetch()) { return false; }
+
+	return true;
+}
+
+// キャラクタパラメータ書き込み
+bool DBConnection::SaveCharacterParameter(int Id, int MaxHp, int Atk, int Def, int Exp, int AreaId, float X, float Y)
+{
+	MySqlQuery Query = Connection.CreateQuery("update CharacterData set MaxHp = ?, Atk = ?, Def = ?, Exp = ?, LastArea = ?, LastX = ?, LastY = ? where CustomerId = ?");
+
+	Query.BindInt(&MaxHp);
+	Query.BindInt(&Atk);
+	Query.BindInt(&Def);
+	Query.BindInt(&Exp);
+	Query.BindInt(&AreaId);
+	Query.BindFloat(&X);
+	Query.BindFloat(&Y);
+	Query.BindInt(&Id);
+
+	if (!Query.ExecuteQuery()) { return false; }
+
+	return true;
+}
+
+// 最後にログアウトした位置を読み込み
+bool DBConnection::ReadLastLogoutPosition(int Id, u32 &OutAreaId, float &OutX, float &OutY)
+{
+	MySqlQuery Query = Connection.CreateQuery("select LastArea, LastX, LastY from CharacterData where CustomerId = ?");
+
+	Query.BindInt(&Id);
+	Query.BindResultInt(&OutAreaId);
+	Query.BindResultFloat(&OutX);
+	Query.BindResultFloat(&OutY);
+
+	if (!Query.ExecuteQuery()) { return false; }
+	if (!Query.Fetch()) { return false; }
+
+	return true;
+}
+
+
+// ユーザデータ登録.
+bool DBConnection::RegisterUserData(char *pUserCode)
+{
+	// ユーザデータ
+	MySqlQuery Query = Connection.CreateQuery("insert into UserData(UserCode) Values(?)");
+	Query.BindString(pUserCode);
+	if (!Query.ExecuteQuery()) { return false; }
+	Query.Close();
+
+	// キャラデータ
+	MySqlQuery UserQuery = Connection.CreateQuery("select Id from UserData where UserCode = ?;");
+	UserQuery.BindString(pUserCode);
+
+	int Id = 0;
+	UserQuery.BindResultInt(&Id);
+	if (!UserQuery.ExecuteQuery()) { return false; }
+	if (!UserQuery.Fetch()) { return false; }
+	UserQuery.Close();
+
+	MySqlQuery CharacterQuery = Connection.CreateQuery("insert into CharacterData values(?, 50, 10, 10, 0, 1, -1000.0, 0.0);");
+	CharacterQuery.BindInt(&Id);
+	if (!CharacterQuery.ExecuteQuery()) { return false; }
+
+	return true;
+}
---
+++ Server/CacheServer/DBConnection.h
@@ -0,0 +1,59 @@
+#ifndef __DBCONNECTION_H__
+#define __DBCONNECTION_H__
+
+#include "MySQL/MySqlConnection.h"
+
+/**
+ * DB接続クラス
+ */
+class DBConnection : noncopyable
+{
+
+public:
+
+	// 開く.
+	bool Open();
+
+	// ユーザデータ読み込み
+	bool LoadUserData(char *pUserCode, int &OutId);
+
+	// キャラクタパラメータ読み込み
+	bool LoadCharacterParameter(int Id, int &OutMaxHp, int &OutAtk, int &OutDef, int &OutExp);
+
+	// キャラクタパラメータ書き込み
+	bool SaveCharacterParameter(int Id, int MaxHp, int Atk, int Def, int Exp, int AreaId, float X, float Y);
+
+	// 最後にログアウトした位置を読み込み
+	bool ReadLastLogoutPosition(int Id, u32 &OutAreaId, float &OutX, float &OutY);
+
+private:
+
+	// ユーザ情報を格納したファイル名.
+	static const std::string UserDataFileName;
+
+	// DBのホスト
+	static const char *DBHost;
+
+	// データベース名.
+	static const char *DBName;
+
+	// 接続.
+	MySqlConnection Connection;
+
+
+	// ユーザデータ登録.
+	bool RegisterUserData(char *pUserCode);
+
+	// ======== Singleton ===========
+public:
+
+	static DBConnection &GetInstance() { return Instance; }
+
+private:
+
+	DBConnection() {}
+	static DBConnection Instance;
+
+};
+
+#endif		// #ifndef __DBCONNECTION_H__
