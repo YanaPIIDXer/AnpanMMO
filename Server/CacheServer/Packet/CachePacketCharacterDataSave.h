29d9d4c740801d4f29f471ffcd8d2e52318966a9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-17 16:36:38 +0900

    キャラクタ情報保存パケット定義。

---
+++ Server/CacheServer/Packet/CachePacketCharacterDataSave.h
@@ -0,0 +1,58 @@
+#ifndef __CACHEPACKETCHARACTERDATASAVE_H__
+#define __CACHEPACKETCHARACTERDATASAVE_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+#include "CachePacketBase.h"
+
+
+class CachePacketCharacterDataSave  : public CachePacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return CacheCharacterDataSave; }
+
+	
+
+	s32 MaxHp;
+	s32 Atk;
+	s32 Def;
+	s32 Exp;
+	u32 LastAreaId;
+	float LastX;
+	float LastY;
+	
+
+	CachePacketCharacterDataSave()
+	{
+	}
+
+	CachePacketCharacterDataSave(u32 InClientId, s32 InMaxHp, s32 InAtk, s32 InDef, s32 InExp, u32 InLastAreaId, float InLastX, float InLastY)
+	{
+		ClientId = InClientId;
+		MaxHp = InMaxHp;
+		Atk = InAtk;
+		Def = InDef;
+		Exp = InExp;
+		LastAreaId = InLastAreaId;
+		LastX = InLastX;
+		LastY = InLastY;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		CachePacketBase::Serialize(pStream);
+		pStream->Serialize(&MaxHp);
+		pStream->Serialize(&Atk);
+		pStream->Serialize(&Def);
+		pStream->Serialize(&Exp);
+		pStream->Serialize(&LastAreaId);
+		pStream->Serialize(&LastX);
+		pStream->Serialize(&LastY);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __CACHEPACKETCHARACTERDATASAVE_H__
