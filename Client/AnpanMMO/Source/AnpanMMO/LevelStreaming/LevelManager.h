00dd73816634206a6011206f6a73960df02bb38b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-19 00:43:11 +0900

    上げ忘れ。

---
+++ Client/AnpanMMO/Source/AnpanMMO/LevelStreaming/LevelManager.h
@@ -0,0 +1,54 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Engine/LevelStreamingDynamic.h"
+#include "LevelManager.generated.h"
+
+DECLARE_DELEGATE(FOnLevelLoadFinished);
+
+/**
+ * レベル管理.
+ */
+UCLASS()
+class ANPANMMO_API ULevelManager : public UObject
+{
+
+	GENERATED_BODY()
+
+public:
+
+	// コンストラクタ
+	ULevelManager(const FObjectInitializer &ObjectInitializer);
+
+	// デストラクタ
+	virtual ~ULevelManager() {}
+
+	// ワールドを設定
+	void SetWorld(UWorld *pInWorld) { pWorld = pInWorld; }
+
+	// 毎フレームの処理.
+	void Poll();
+
+	// ロード
+	void Load(const FString &LevelPath);
+
+
+	// レベルロードが完了した。
+	FOnLevelLoadFinished OnLevelLoadFinished;
+
+private:
+
+	// World
+	TWeakObjectPtr<UWorld> pWorld;
+	
+	// レベルストリーミング
+	UPROPERTY()
+	ULevelStreamingDynamic *pLevelStreaming;
+
+	// ロードが完了したか？
+	bool bIsLoaded;
+
+};
