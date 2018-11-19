16d89a9eedc133befdff1a3dad940aa8495b3d7d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-19 13:27:40 +0900

    Pakファイル管理クラスを追加。

---
+++ Client/AnpanMMO/Source/AnpanMMO/DLC/PakFileManager.cpp
@@ -0,0 +1,49 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#include "PakFileManager.h"
+#include "HAL/PlatformFilemanager.h"
+
+PakFileManager PakFileManager::Instance;
+
+// コンストラクタ
+PakFileManager::PakFileManager()
+	: pPakPlatform(nullptr)
+{
+}
+
+// デストラクタ
+PakFileManager::~PakFileManager()
+{
+#if !WITH_EDITOR
+	delete pPakPlatform;
+	pPakPlatform = nullptr;
+#endif
+}
+
+// 初期化.
+void PakFileManager::Initialize()
+{
+#if !WITH_EDITOR
+	if (pPakPlatform != nullptr) { return; }
+
+	IPlatformFile &InnerPlatform = FPlatformFileManager::Get().GetPlatformFile();
+	pPakPlatform = new FPakPlatformFile();
+	pPakPlatform->Initialize(&InnerPlatform, TEXT(""));
+	FPlatformFileManager::Get().SetPlatformFile(*pPakPlatform);
+#endif
+}
+
+// マウント
+void PakFileManager::Mount(const FString &Path)
+{
+#if !WITH_EDITOR
+	// マウント済み。
+	if (MountedPaths.Find(Path)) { return; }
+	if (!pPakPlatform->Mount(*Path, 0, *FPaths::GameContentDir()))
+	{
+		UE_LOG(LogTemp, Log, TEXT("%s Mount Failed..."), *Path);
+		return;
+	}
+	MountedPaths.Add(Path);
+#endif
+}
