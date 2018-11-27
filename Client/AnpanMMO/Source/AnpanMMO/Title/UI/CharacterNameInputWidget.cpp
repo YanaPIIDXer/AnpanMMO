96b8414b388c04c7fd7751d748a78a919d2c2471
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-27 18:31:37 +0900

    パケット送信処理を実装。

--- Client/AnpanMMO/Source/AnpanMMO/Title/UI/CharacterNameInputWidget.cpp
+++ Client/AnpanMMO/Source/AnpanMMO/Title/UI/CharacterNameInputWidget.cpp
@@ -1,6 +1,10 @@
 // Copyright 2018 YanaPIIDXer All Rights Reserved.
 
 #include "CharacterNameInputWidget.h"
+#include "StringPack.h"
+#include "Packet/PacketCreateCharacterRequest.h"
+#include "Kismet/GameplayStatics.h"
+#include "MMOGameInstance.h"
 
 // コンストラクタ
 UCharacterNameInputWidget::UCharacterNameInputWidget(const FObjectInitializer &ObjectInitializer)
@@ -12,5 +16,11 @@ UCharacterNameInputWidget::UCharacterNameInputWidget(const FObjectInitializer &O
 // リクエストパケット送信.
 void UCharacterNameInputWidget::SendRequestPacket(const FString &Name)
 {
+	StringPack<32> NamePack(TCHAR_TO_UTF8(*Name));
+	PacketCreateCharacterRequest Packet(NamePack);
 
+	auto *pInst = Cast<UMMOGameInstance>(UGameplayStatics::GetGameInstance(this));
+	check(pInst != nullptr);
+
+	pInst->SendPacket(&Packet);
 }
