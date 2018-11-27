5a5a8a1be9a4c29dd807dc613db636158f47b00b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-27 17:11:29 +0900

    クライアント、キャッシュサーバにもStringPackクラスを追加。

---
+++ Server/CacheServer/StringPack.h
@@ -0,0 +1,82 @@
+#ifndef __STRINGPACK_H__
+#define __STRINGPACK_H__
+
+#include "MemoryStream/MemoryStreamInterface.h"
+
+/**
+ * 文字列パック
+ */
+template<std::size_t SIZE>
+class StringPack
+{
+
+public:
+
+	// コンストラクタ
+	StringPack()
+	{
+		pString = new char[SIZE];
+	}
+
+	StringPack(char *pStr)
+	{
+		pString = new char[SIZE];
+		Set(pStr);
+	}
+
+	// デストラクタ
+	~StringPack()
+	{
+		delete[] pString;
+	}
+
+	// 文字列をセット.
+	void Set(char *pStr)
+	{
+		memcpy(pString, pStr, strlen(pStr));
+	}
+
+	// 取得.
+	const char *Get() const { return pString; }
+
+	// 文字列長を取得.
+	u32 GetLength() const
+	{
+		u32 Length = 0;
+		for (int i = 0; i < SIZE; i++)
+		{
+			if (pString[i] == '\0') { break; }
+			Length += GetByte(pString[i]);
+		}
+		return Length;
+	}
+
+	// シリアライズ
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		if (!pStream->Serialize(pString)) { return false; }
+		return true;
+	}
+
+private:
+
+	// 文字列.
+	char *pString;
+
+
+	// バイト数を取得.
+	u32 GetByte(char Ch)
+	{
+		if (Ch >= 0x00 && Ch <= 0x7f) { return 1; }
+		if (Ch >= 0xc2 && Ch <= 0xdf) { return 2; }
+		if (Ch >= 0xe0 && Ch <= 0xef) { return 3; }
+		if (Ch >= 0xf0 && Ch <= 0xf7) { return 4; }
+		if (Ch >= 0xf8 && Ch <= 0xfb) { return 5; }
+		if (Ch >= 0xfc && Ch <= 0xfd) { return 6; }
+
+		return 0;
+	}
+
+};
+
+#endif		// #ifndef __STRINGPACK_H__
