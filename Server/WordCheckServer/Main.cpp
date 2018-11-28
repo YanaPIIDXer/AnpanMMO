dfa5a0eba07529b775153f84b218811869f5745f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-28 17:16:58 +0900

    ゲームサーバの接続を待ち受けるようにした。

--- Server/WordCheckServer/Main.cpp
+++ Server/WordCheckServer/Main.cpp
@@ -1,8 +1,15 @@
 #include "stdafx.h"
-
+#include "GameServerConnection.h"
 
 // エントリポイント
 int main()
 {
+	asio::io_service IOService;
+
+	tcp::socket *pSock = new tcp::socket(IOService);
+	shared_ptr<tcp::socket> pSocket = shared_ptr<tcp::socket>(pSock);
+	GameServerConnection GameServerConn(IOService, pSocket);
+
+	IOService.run();
 	return 0;
 }
