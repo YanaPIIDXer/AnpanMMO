d030aef9b2fc292743f4a7f0cb44683263adb180
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-29 00:10:38 +0900

    大文字・小文字を考慮して判定するようにした。

--- Server/WordCheckServer/WordChecker.cpp
+++ Server/WordCheckServer/WordChecker.cpp
@@ -2,6 +2,7 @@
 #include "WordChecker.h"
 #include "MySQL/MySqlConnection.h"
 #include <fstream>
+#include <algorithm>
 
 const std::string WordChecker::MasterDBInfoFileName = "MasterDBUserData.txt";
 WordChecker WordChecker::Instance;
@@ -17,7 +18,11 @@ std::string WordChecker::ChatWordCheck(const std::string &Message)
 		// @TODO;愚直な判定。
 		//		 全角半角を考慮する方法ってある・・・？
 		std::string NGWord = WordList[i].NGWord;
-		size_t Pos = Result.find(NGWord);
+		std::string Tmp1 = Result;
+		std::string Tmp2 = NGWord;
+		std::transform(Tmp1.begin(), Tmp1.end(), Tmp1.begin(), toupper);
+		std::transform(Tmp2.begin(), Tmp2.end(), Tmp2.begin(), toupper);
+		size_t Pos = Tmp1.find(Tmp2);
 		if (Pos == std::string::npos) { continue; }
 		size_t Length = NGWord.length();
 		std::string Replace = "";
