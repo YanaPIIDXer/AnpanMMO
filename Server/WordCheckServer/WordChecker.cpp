bd44f0e84825f22255e81f18df8057393b774c3f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-28 23:59:55 +0900

    不要になったログ出力を撤去。

--- Server/WordCheckServer/WordChecker.cpp
+++ Server/WordCheckServer/WordChecker.cpp
@@ -17,7 +17,6 @@ std::string WordChecker::ChatWordCheck(const std::string &Message)
 		// @TODO;愚直な判定。
 		//		 全角半角を考慮する方法ってある・・・？
 		std::string NGWord = WordList[i].NGWord;
-		std::cout << NGWord << std::endl;
 		size_t Pos = Result.find(NGWord);
 		if (Pos == std::string::npos) { continue; }
 		size_t Length = NGWord.length();
@@ -57,12 +56,6 @@ bool WordChecker::LoadMaster()
 	{
 		std::cout << "NGWord Master Load Failed..." << std::endl;
 		return false;
-	}
-
-	std::vector<NGWordItem> WordList = NGWords.GetAll();
-	for (unsigned int i = 0; i < WordList.size(); i++)
-	{
-		std::cout << "NGWord:" << WordList[i].NGWord << std::endl;
 	}
 
 	std::cout << "WordChecker LoadMaster Success!!" << std::endl;
