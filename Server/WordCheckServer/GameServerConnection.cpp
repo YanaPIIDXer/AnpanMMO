d9c5a2578dd1255b35d8f79467be31e3913b426c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-28 17:12:07 +0900

    GameServerConnectionクラス、PacketReceiverクラスをキャッシュサーバから失敬。

---
+++ Server/WordCheckServer/GameServerConnection.cpp
@@ -0,0 +1,81 @@
+#include "stdafx.h"
+#include "GameServerConnection.h"
+#include "Config.h"
+#include "Packet/PacketBase.h"
+#include "MemoryStream/MemorySizeCaliculateStream.h"
+#include "MemoryStream/MemoryStreamWriter.h"
+#include "MemoryStream/MemoryStreamReader.h"
+#include "Packet/PacketHeader.h"
+
+// コンストラクタ
+GameServerConnection::GameServerConnection(asio::io_service &IOService, const shared_ptr<tcp::socket> &pInSocket)
+	: TCPConnection(pInSocket)
+	, Acceptor(IOService, tcp::endpoint(tcp::v4(), Config::WordCheckServerPort))
+	, Receiver(this)
+{
+	Accept();
+}
+
+// パケット送信.
+void GameServerConnection::SendPacket(PacketBase *pPacket)
+{
+	//まずはサイズを求める
+	MemorySizeCaliculateStream SizeStream;
+	pPacket->Serialize(&SizeStream);
+
+	//シリアライズ本番
+	MemoryStreamWriter WriteStream(SizeStream.GetSize() + 3);
+
+	u8 Id = (u8)pPacket->GetPacketID();
+	u16 Size = SizeStream.GetSize();
+	WriteStream.Serialize(&Id);
+	WriteStream.Serialize(&Size);
+	pPacket->Serialize(&WriteStream);
+
+	//送信
+	AsyncSend(WriteStream.GetStream(), WriteStream.GetSize());
+}
+
+
+// データを受信した。
+void GameServerConnection::OnRecvData(size_t Size)
+{
+	u8 *pRecvData = RecvBuffer.GetTop();
+	MemoryStreamReader ReadStream(pRecvData, Size);
+	PacketHeader Header;
+	if (Header.Serialize(&ReadStream) && RecvBuffer.GetSize() >= Header.GetPacketSize() + 3)
+	{
+		RecvBuffer.Pop(3);
+
+		MemoryStreamReader BodyStream(RecvBuffer.GetTop(), Header.GetPacketSize());
+		Receiver.RecvPacket(Header.GetPacketId(), &BodyStream);
+		
+		RecvBuffer.Pop(Header.GetPacketSize());
+	}
+}
+
+// 切断された。
+void GameServerConnection::OnDisconnected()
+{
+	GetSocket()->close();
+	Accept();
+}
+
+
+// Acceptする。
+void GameServerConnection::Accept()
+{
+	Acceptor.async_accept(*GetSocket(), bind(&GameServerConnection::OnAccept, this, asio::placeholders::error));
+}
+
+// Acceptした。
+void GameServerConnection::OnAccept(const boost::system::error_code &ErrorCode)
+{
+	if (ErrorCode)
+	{
+		std::cout << ErrorCode.message() << std::endl;
+		return;
+	}
+	bIsConnected = true;
+	AsyncRecv();
+}
