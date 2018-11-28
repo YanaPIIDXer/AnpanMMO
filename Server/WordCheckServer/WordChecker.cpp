3c6786eed086f5466d9de7670782f323e80eecef
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-29 00:57:13 +0900

    連続して同じＮＧワードが入力されているケース（例えば「FuckFuck」等）を考慮するようにした。

--- Server/WordCheckServer/WordChecker.cpp
+++ Server/WordCheckServer/WordChecker.cpp
@@ -15,22 +15,24 @@ std::string WordChecker::ChatWordCheck(const std::string &Message)
 	std::vector<NGWordItem> WordList = NGWords.GetAll();
 	for (unsigned int i = 0; i < WordList.size(); i++)
 	{
-		// @TODO;愚直な判定。
-		//		 全角半角を考慮する方法ってある・・・？
 		std::string NGWord = WordList[i].NGWord;
 		std::string Tmp1 = Result;
 		std::string Tmp2 = NGWord;
 		std::transform(Tmp1.begin(), Tmp1.end(), Tmp1.begin(), toupper);
 		std::transform(Tmp2.begin(), Tmp2.end(), Tmp2.begin(), toupper);
 		size_t Pos = Tmp1.find(Tmp2);
-		if (Pos == std::string::npos) { continue; }
-		size_t Length = NGWord.length();
-		std::string Replace = "";
-		for (size_t j = 0; j < Length; j++)
+		while (Pos != std::string::npos)
 		{
-			Replace += "*";
+			size_t Length = NGWord.length();
+			std::string Replace = "";
+			for (size_t j = 0; j < Length; j++)
+			{
+				Replace += "*";
+			}
+			Result = Result.replace(Pos, Length, Replace);
+			Tmp1 = Tmp1.replace(Pos, Length, Replace);
+			Pos = Tmp1.find(Tmp2);
 		}
-		Result = Result.replace(Pos, Length, Replace);
 	}
 
 	return Result;
