2e7d52855f370b010429a125e77b44ccb56de226
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-29 22:29:20 +0900

    パーティ勧誘結果パケットを追加。 ※勧誘レスポンスパケットとは別物。

---
+++ Server/AnpanMMOServer/Packet/PacketPartyInviteResult.h
@@ -0,0 +1,41 @@
+#ifndef __PACKETPARTYINVITERESULT_H__
+#define __PACKETPARTYINVITERESULT_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketPartyInviteResult  : public PacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return PartyInviteResult; }
+
+	enum ResultCode
+	{
+		Success,
+		Error,
+	};
+
+	u8 Result;
+	
+
+	PacketPartyInviteResult()
+	{
+	}
+
+	PacketPartyInviteResult(u8 InResult)
+	{
+		Result = InResult;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&Result);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETPARTYINVITERESULT_H__
