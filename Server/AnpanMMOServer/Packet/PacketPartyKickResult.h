676e88136076a3c8718ba04564df6f0c6b9a2d03
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-11-30 12:14:49 +0900

    キック関係パケット追加。

---
+++ Server/AnpanMMOServer/Packet/PacketPartyKickResult.h
@@ -0,0 +1,41 @@
+#ifndef __PACKETPARTYKICKRESULT_H__
+#define __PACKETPARTYKICKRESULT_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketPartyKickResult  : public PacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return PartyKickResult; }
+
+	enum ResultCode
+	{
+		Success,
+		Error,
+	};
+
+	u8 Result;
+	
+
+	PacketPartyKickResult()
+	{
+	}
+
+	PacketPartyKickResult(u8 InResult)
+	{
+		Result = InResult;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&Result);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETPARTYKICKRESULT_H__
