c54a0b7cd08bb6fe7bf5a487a0dc4ec3654ab16b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-02 00:05:46 +0900

    lua一式追加。 ※Android、iOS向けのライブラリファイルはLinux版から失敬。動くかどうかは知らん。

--- Client/AnpanMMO/Source/AnpanMMO/AnpanMMO.Build.cs
+++ Client/AnpanMMO/Source/AnpanMMO/AnpanMMO.Build.cs
@@ -1,9 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class AnpanMMO : ModuleRules
 {
+	private string ThirdPartyPath
+	{
+		get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../Source/ThirdParty/")); }
+	}
+
 	public AnpanMMO(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -14,10 +20,39 @@ public class AnpanMMO : ModuleRules
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		LoadLua(Target);
+	}
+
+	private bool LoadLua(ReadOnlyTargetRules Target)
+	{
+
+		string PlatformString = ".so";
+		switch(Target.Platform)
+		{
+			case UnrealTargetPlatform.Win64:
+
+				PlatformString = ".x64.lib";
+				break;
+
+			case UnrealTargetPlatform.Win32:
+
+				PlatformString = ".x86.lib";
+				break;
+		}
+
+		string LibrariesPath = Path.Combine(ThirdPartyPath, "Lua", "libraries");
+
+		PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "Lua53" + PlatformString));
+
+		PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Lua", "includes"));
+
+		PublicDefinitions.Add(string.Format("WITH_LUA_BINDING={0}", 1));
+
+		return true;
 	}
 }
