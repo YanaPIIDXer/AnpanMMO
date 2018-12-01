5f166dcbe8fcd028b3653f40fbbf243982264845
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-01 23:35:21 +0900

    includeをlua.hppに戻してコミット。

--- Server/AnpanMMOServer/Main.cpp
+++ Server/AnpanMMOServer/Main.cpp
@@ -10,7 +10,7 @@
 #include "CacheServer/CacheServerConnection.h"
 #include "WordCheckServer/WordCheckServerConnection.h"
 
-#include "lua.h"
+#include "lua.hpp"
 #include "lauxlib.h"
 #include "lualib.h"
 
