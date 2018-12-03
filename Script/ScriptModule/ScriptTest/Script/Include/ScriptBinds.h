9eef3de357d261d96adfe9a927aee046d1014246
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-04 08:08:32 +0900

    追加されていなかったものを追加。

---
+++ Script/ScriptModule/ScriptTest/Script/Include/ScriptBinds.h
@@ -0,0 +1,17 @@
+#ifndef __SCRIPTBINDS_H__
+#define __SCRIPTBINDS_H__
+
+/**
+ * Luaにバインドする関数群定義。
+ * クラスのメンバ関数がバインドできない為の処置。
+ * ※ここに定義された関数はC++側からは呼び出さない事。
+ */
+
+#include "lua.hpp"
+#include "lauxlib.h"
+#include "lualib.h"
+
+// メッセージ表示.
+int ShowMessage_Call(lua_State *pState);
+
+#endif		// #ifndef __SCRIPTBINDS_H__
