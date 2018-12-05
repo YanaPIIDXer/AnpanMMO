fdf0deb68ec6d7a46b6fe50566faf79253cff651
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-05 19:24:25 +0900

    ビルドとデプロイを行うバッチファイルを作成。 ひとまずWindows向けのビルドのみ。

---
+++ Script/ScriptModule/BuildAndDeploy.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Windows x86
+devenv /rebuild "release|x86" /out BuildLog/build.log ScriptModule\ScriptModule.vcxproj /project ScriptModule
+
+rem Windows x64
+devenv /rebuild "release|x64" /out BuildLog/build.log ScriptModule\ScriptModule.vcxproj /project ScriptModule
+
+pause
