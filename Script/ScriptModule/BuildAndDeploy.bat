5e7219ab3496838c8fd3c7cffde07164df49e885
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-06 03:22:45 +0900

    Linux、モバイル向けのビルドを行えるようにした。

--- Script/ScriptModule/BuildAndDeploy.bat
+++ Script/ScriptModule/BuildAndDeploy.bat
@@ -6,4 +6,8 @@ devenv /rebuild "release|x86" /out BuildLog/build.log ScriptModule\ScriptModule.
 rem Windows x64
 devenv /rebuild "release|x64" /out BuildLog/build.log ScriptModule\ScriptModule.vcxproj /project ScriptModule
 
+rem Linux and Mobiles
+GenerateMakeFile
+make
+
 pause
