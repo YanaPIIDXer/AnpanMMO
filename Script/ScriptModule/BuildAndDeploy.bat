3118c679e79fe4c8a9e56f062fc5d3de2a5bf760
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-06 03:37:38 +0900

    ヘッダとライブラリファイルをクライアントとサーバにデプロイするようにした。

--- Script/ScriptModule/BuildAndDeploy.bat
+++ Script/ScriptModule/BuildAndDeploy.bat
@@ -10,4 +10,19 @@ rem Linux and Mobiles
 GenerateMakeFile
 make
 
+rem Deploy to Client
+set INCLUDE=ScriptModule\Script\Include\*
+set CLIENT_MODULE_ROOT=..\..\Client\AnpanMMO\Source\ScriptModule\
+set CLIENT_INCLUDE=%CLIENT_MODULE_ROOT%Include\
+set CLIENT_LIBRARY=%CLIENT_MODULE_ROOT%Libraries\
+copy %INCLUDE% %CLIENT_INCLUDE%
+copy ScriptModule_x64.lib %CLIENT_LIBRARY%
+copy ScriptModule_x86.lib %CLIENT_LIBRARY%
+copy libscriptmodule.a %CLIENT_LIBRARY%
+
+rem Deploy to Server
+set SERVER_DIR=..\..\Server\AnpanMMOServer\
+copy %INCLUDE% %SERVER_DIR%\Script\
+copy libscriptmodule.a %SERVER_DIR%
+
 pause
