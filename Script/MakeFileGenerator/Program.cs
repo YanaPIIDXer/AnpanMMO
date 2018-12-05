9fbf5ab5ea6c59e99f6b9bd81c23c5fb5b0372ea
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-06 01:47:50 +0900

    makefile自動生成ツールを作成。 ※何故かWindows環境だとルールにワイルドカード（例えば$(OBJROOT)\%.oみたいな書き方）がエラーになるため。

---
+++ Script/MakeFileGenerator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MakeFileGenerator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string MakeFile = "";
+			using(StreamReader Reader = new StreamReader("makefile_base.txt"))
+			{
+				MakeFile = Reader.ReadToEnd();
+			}
+
+			MakeFile += "\n";
+
+			string[] Sources = Directory.GetFiles("ScriptModule", "*.cpp", SearchOption.AllDirectories);
+			foreach(var Source in Sources)
+			{
+				string ObjFilePath = ".\\obj\\" + Path.GetDirectoryName(Source) + "\\" + Path.GetFileNameWithoutExtension(Source) + ".o";
+				MakeFile += ObjFilePath + ": " + Source + "\n";
+				MakeFile += "\tif not exist " + Path.GetDirectoryName(ObjFilePath) + " mkdir " + Path.GetDirectoryName(ObjFilePath) + "\n";
+				MakeFile += "\t$(COMPILER) -MD $(INCLUDE) -o $@ -c $<\n\n";
+			}
+
+			using (StreamWriter Writer = new StreamWriter("makefile"))
+			{
+				Writer.Write(MakeFile);
+			}
+		}
+	}
+}
