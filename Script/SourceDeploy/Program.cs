fe8aea3ccad73592fa05cddda25b5a8b113b6f6a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-06 15:46:59 +0900

    ソースコードをデプロイ＆クライアントへのデプロイ時は#include "stdafx.h"を消す処理を行うツールを作成。

---
+++ Script/SourceDeploy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SourceDeploy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string SrcPath = "ScriptModule\\Script\\";
+			string ServerPath = "..\\..\\Server\\AnpanMMOServer\\Script\\";
+			string ClientPath = "..\\..\\Client\\AnpanMMO\\Source\\ScriptModule\\";
+
+			string[] Sources = Directory.GetFiles(SrcPath, "*.*", SearchOption.AllDirectories);
+			foreach(var Source in Sources)
+			{
+				// サーバへコピー
+				string FileName = Path.GetFileName(Source);
+				File.Copy(Source, ServerPath + FileName, true);
+
+				// クライアントへコピー
+				File.Copy(Source, ClientPath + FileName, true);
+				if(Path.GetExtension(Source) == ".cpp")
+				{
+					// cppファイルだった場合は先頭の#include "stdafx"を除去。
+					string Code = "";
+					using (StreamReader Reader = new StreamReader(ClientPath + FileName, Encoding.GetEncoding("Shift-Jis")))
+					{
+						Code = Reader.ReadToEnd();
+					}
+					Code = Code.Replace("#include \"stdafx.h\"", "");
+					Code = Code.Substring(2);
+					using (StreamWriter Writer = new StreamWriter(ClientPath + FileName, false, Encoding.GetEncoding("Shift-Jis")))
+					{
+						Writer.Write(Code);
+					}
+				}
+			}
+
+		}
+	}
+}
