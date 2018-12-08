89b7e8aede2c9469d11c7ce9a983e948a5a31aa0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-09 08:16:56 +0900

    NPC関係パケット追加。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Packet/PacketNPCTalkSelection.h
@@ -0,0 +1,37 @@
+#ifndef __PACKETNPCTALKSELECTION_H__
+#define __PACKETNPCTALKSELECTION_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketNPCTalkSelection  : public PacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return NPCTalkSelection; }
+
+	
+
+	s32 Index;
+	
+
+	PacketNPCTalkSelection()
+	{
+	}
+
+	PacketNPCTalkSelection(s32 InIndex)
+	{
+		Index = InIndex;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&Index);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETNPCTALKSELECTION_H__
---
+++ Server/AnpanMMOServer/Packet/PacketNPCTalk.h
@@ -0,0 +1,37 @@
+#ifndef __PACKETNPCTALK_H__
+#define __PACKETNPCTALK_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketNPCTalk  : public PacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return NPCTalk; }
+
+	
+
+	u32 MasterId;
+	
+
+	PacketNPCTalk()
+	{
+	}
+
+	PacketNPCTalk(u32 InMasterId)
+	{
+		MasterId = InMasterId;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&MasterId);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETNPCTALK_H__
