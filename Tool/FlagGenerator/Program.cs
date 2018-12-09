c66f4119e31b700619e9597bc79d2f59c5298951
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-09 09:08:58 +0900

    サーバ、クライアントにenumを定義したヘッダを出力するようにした。

--- Tool/FlagGenerator/Program.cs
+++ Tool/FlagGenerator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FlagGenerator
 {
@@ -10,7 +11,28 @@ namespace FlagGenerator
 	{
 		static void Main(string[] args)
 		{
+			ExcelParser Parser = new ExcelParser();
+			if(!Parser.Read())
+			{
+				Console.WriteLine("エクセルのパースに失敗しました。");
+				return;
+			}
 
+			// enumソース生成.
+			HeaderGenerator HeaderGen = new HeaderGenerator(Parser.Flags);
+			HeaderGen.Generate();
+
+			string ServerPath = "..\\..\\Server\\AnpanMMOServer\\Script\\ScriptFlag.h";
+			using (StreamWriter Writer = new StreamWriter(ServerPath, false))
+			{
+				Writer.Write(HeaderGen.Code);
+			}
+
+			string ClientPath = "..\\..\\Client\\AnpanMMO\\Source\\AnpanMMO\\Script\\ScriptFlag.h";
+			using (StreamWriter Writer = new StreamWriter(ClientPath, false))
+			{
+				Writer.Write(HeaderGen.Code);
+			}
 		}
 	}
 }
