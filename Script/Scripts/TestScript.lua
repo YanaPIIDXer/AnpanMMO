146abded14262a420cc7f7c110b23df8c02f96d8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-09 12:03:28 +0900

    修正。

--- Script/Scripts/TestScript.lua
+++ Script/Scripts/TestScript.lua
@@ -5,9 +5,9 @@ PushSelection("選択肢３")
 ShowSelection()
 
 if (Selected == 0) then
-	ShowMessage("選択肢１です。)
+	ShowMessage("選択肢１です。")
 	local bFlag = GetFlag(Test1)
-	if (!bFlag) then
+	if (not bFlag) then
 		ShowMessage("フラグが立っていないので立てます。")
 		SetFlag(Test1)
 	else
@@ -16,7 +16,7 @@ if (Selected == 0) then
 elseif (Selected == 1) then
 	ShowMessage("選択肢２です。)
 	local bFlag = GetFlag(Test2)
-	if (!bFlag) then
+	if (not bFlag) then
 		ShowMessage("フラグが立っていないので立てます。")
 		SetFlag(Test2)
 	else
@@ -25,7 +25,7 @@ elseif (Selected == 1) then
 else
 	ShowMessage("選択肢３です。)
 	local bFlag = GetFlag(Test3)
-	if (!bFlag) then
+	if (not bFlag) then
 		ShowMessage("フラグが立っていないので立てます。")
 		SetFlag(Test3)
 	else
