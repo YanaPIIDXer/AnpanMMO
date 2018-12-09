0e2b48986e0cd7b6fd1654f24eab7fe4590cee48
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-09 12:08:39 +0900

    close #144 スクリプトの通信処理対応。

--- Client/AnpanMMO/Source/AnpanMMO/Active/UI/Script/ScriptSelectionWidget.cpp
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Script/ScriptSelectionWidget.cpp
@@ -4,6 +4,7 @@
 #include "Util.h"
 #include "MMOGameInstance.h"
 #include "Kismet/GameplayStatics.h"
+#include "Packet/PacketNPCTalkSelection.h"
 
 const TCHAR *UScriptSelectionWidget::AssetPath = TEXT("/Game/Blueprints/UI/Active/Script/SelectionWidget.SelectionWidget");
 
@@ -41,6 +42,9 @@ void UScriptSelectionWidget::Select(int32 Index)
 {
 	UMMOGameInstance *pInst = Cast<UMMOGameInstance>(UGameplayStatics::GetGameInstance(this));
 	check(pInst != nullptr);
+
+	PacketNPCTalkSelection Packet(Index);
+	pInst->SendPacket(&Packet);
 
 	pInst->GetScript()->OnSelectedSelection(Index);
 	Back();
