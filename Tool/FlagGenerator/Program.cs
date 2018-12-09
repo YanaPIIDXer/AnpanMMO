3e741a6a05de846009c54ffd29c86b8d59956956
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-09 11:21:41 +0900

    ヘッダファイルの出力は不要なので撤去。

--- Tool/FlagGenerator/Program.cs
+++ Tool/FlagGenerator/Program.cs
@@ -18,25 +18,7 @@ namespace FlagGenerator
 				Console.ReadKey();
 				return;
 			}
-
-			// enumソース生成.
-			HeaderGenerator HeaderGen = new HeaderGenerator(Parser.Flags);
-			HeaderGen.Generate();
-
-			string ServerPath = "..\\..\\Server\\AnpanMMOServer\\Script\\ScriptFlag.h";
-			using (StreamWriter Writer = new StreamWriter(ServerPath, false))
-			{
-				Writer.Write(HeaderGen.Code);
-			}
-
-			string ClientPath = "..\\..\\Client\\AnpanMMO\\Source\\AnpanMMO\\Script\\ScriptFlag.h";
-			using (StreamWriter Writer = new StreamWriter(ClientPath, false))
-			{
-				Writer.Write(HeaderGen.Code);
-			}
-
-			Console.WriteLine("ソースファイルを生成しました。");
-
+			
 			ScriptGenerator ScriptGen = new ScriptGenerator(Parser.Flags);
 			if(!ScriptGen.Generate())
 			{
