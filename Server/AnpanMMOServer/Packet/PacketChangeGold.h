9f512815209f35a2a6ecc8770fa431487ae986ab
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-12 20:08:46 +0900

    ゴールド変化パケット追加。

---
+++ Server/AnpanMMOServer/Packet/PacketChangeGold.h
@@ -0,0 +1,37 @@
+#ifndef __PACKETCHANGEGOLD_H__
+#define __PACKETCHANGEGOLD_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketChangeGold  : public PacketBase
+{
+public:
+	virtual PacketID GetPacketID() const { return ChangeGold; }
+
+	
+
+	u32 Gold;
+	
+
+	PacketChangeGold()
+	{
+	}
+
+	PacketChangeGold(u32 InGold)
+	{
+		Gold = InGold;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&Gold);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETCHANGEGOLD_H__
