d94d675d92ec97e25ee2e458608cce6f6c08b9fb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-18 12:32:46 +0900

    アイテム使用を実装。

--- Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Item/ItemMenu.cpp
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Item/ItemMenu.cpp
@@ -4,6 +4,10 @@
 #include "Character/Player/GameCharacter.h"
 #include "Kismet/GameplayStatics.h"
 #include "Master/MasterData.h"
+#include "MMOGameInstance.h"
+#include "Packet/PacketItemUse.h"
+#include "Packet/PacketItemSubtractRequest.h"
+#include "Packet/CharacterType.h"
 
 // コンストラクタ
 UItemMenu::UItemMenu(const FObjectInitializer &ObjectInitializer)
@@ -32,4 +36,44 @@ void UItemMenu::Init()
 	}
 
 	InitEvent(Items);
+}
+
+// パケットを受信した。
+void UItemMenu::OnRecvPacket(uint8 ID)
+{
+	switch (ID)
+	{
+		case PacketID::ItemSubtract:
+
+			// アイテム消費したら再構築.
+			Init();
+			break;
+	}
+}
+
+
+// 使用.
+void UItemMenu::Use(const FItemData &Data)
+{
+	auto *pCharacter = Cast<AGameCharacter>(UGameplayStatics::GetPlayerCharacter(this, 0));
+	check(pCharacter != nullptr);
+
+	// メニュー経由の場合は自分自身を対象に使用する。
+	PacketItemUse Packet(Data.ItemId, CharacterType::Player, pCharacter->GetUuid());
+
+	UMMOGameInstance *pInst = Cast<UMMOGameInstance>(UGameplayStatics::GetGameInstance(this));
+	check(pInst != nullptr);
+
+	pInst->SendPacket(&Packet);
+}
+
+// 破棄.
+void UItemMenu::Subtract(const FItemData &Data, int32 Count)
+{
+	PacketItemSubtractRequest Packet(Data.ItemId, Count);
+
+	UMMOGameInstance *pInst = Cast<UMMOGameInstance>(UGameplayStatics::GetGameInstance(this));
+	check(pInst != nullptr);
+
+	pInst->SendPacket(&Packet);
 }
--- Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Item/ItemMenu.h
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Item/ItemMenu.h
@@ -49,11 +49,22 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "ItemMenu")
 	void Init();
 
+	// パケットを受信した。
+	virtual void OnRecvPacket(uint8 ID) override;
+
 protected:
 
 	// 初期化イベント
 	UFUNCTION(BlueprintNativeEvent, Category = "ItemMenu")
 	void InitEvent(const TArray<FItemData> &Items);
 	void InitEvent_Implementation(const TArray<FItemData> &Items) {}
+
+	// 使用.
+	UFUNCTION(BlueprintCallable, Category = "ItemMenu")
+	void Use(const FItemData &Data);
+
+	// 破棄.
+	UFUNCTION(BlueprintCallable, Category = "ItemMenu")
+	void Subtract(const FItemData &Data, int32 Count);
 
 };
