f7f65216a2c5e082170717376893e2be3e427e28
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-18 13:23:56 +0900

    アイテムショートカット関係パケット追加。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Packet/PacketItemShortcut.h
@@ -0,0 +1,40 @@
+#ifndef __PACKETITEMSHORTCUT_H__
+#define __PACKETITEMSHORTCUT_H__
+
+#include "PacketBase.h"
+#include "PacketID.h"
+#include "MemoryStream/MemoryStreamInterface.h"
+
+
+class PacketItemShortcut  : public PacketBase
+{
+public:
+	virtual u8 GetPacketID() const { return PacketID::ItemShortcut; }
+
+	
+
+	u32 ItemId1;
+	u32 ItemId2;
+	
+
+	PacketItemShortcut()
+	{
+	}
+
+	PacketItemShortcut(u32 InItemId1, u32 InItemId2)
+	{
+		ItemId1 = InItemId1;
+		ItemId2 = InItemId2;
+		
+	}
+
+	bool Serialize(MemoryStreamInterface *pStream)
+	{
+		pStream->Serialize(&ItemId1);
+		pStream->Serialize(&ItemId2);
+		
+		return true;
+	}
+};
+
+#endif		// #ifndef __PACKETITEMSHORTCUT_H__
