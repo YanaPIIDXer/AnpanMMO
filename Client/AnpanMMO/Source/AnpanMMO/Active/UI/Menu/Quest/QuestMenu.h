0a59471c45fea8fff09d918273e064c288d5f4bc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-19 23:08:02 +0900

    クエストメニュー追加。 ※内容はまだクエスト名と説明文表示のみ。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Quest/QuestMenu.h
@@ -0,0 +1,67 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Menu/LayeredMenuWidgetBase.h"
+#include "QuestMenu.generated.h"
+
+// クエストデータ
+USTRUCT(BlueprintType)
+struct FQuestData
+{
+
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	// クエストＩＤ
+	uint32 QuestId;
+
+	// クエスト名.
+	UPROPERTY(BlueprintReadOnly)
+	FString Name;
+
+	// 説明文.
+	UPROPERTY(BlueprintReadOnly)
+	FString Explain;
+
+	// メインクエストか？
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsMainQuest;
+
+	// アクティブクエストか？
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsActive;
+
+};
+
+/**
+ * クエストメニュー
+ */
+UCLASS()
+class ANPANMMO_API UQuestMenu : public ULayeredMenuWidgetBase
+{
+
+	GENERATED_BODY()
+	
+public:
+
+	// コンストラクタ
+	UQuestMenu(const FObjectInitializer &ObjectInitializer);
+
+	// デストラクタ
+	virtual ~UQuestMenu() {}
+
+protected:
+
+	// 初期化.
+	UFUNCTION(BlueprintCallable, Category = "QuestMenu")
+	void Init();
+
+	// 初期化イベント
+	UFUNCTION(BlueprintNativeEvent, Category = "QuestMenu")
+	void InitEvent(const TArray<FQuestData> &List);
+	void InitEvent_Implementation(const TArray<FQuestData> &List) {}
+
+};
