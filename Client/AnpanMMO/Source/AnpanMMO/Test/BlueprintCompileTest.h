4f678f7c64a143aa350df89f4f4f8990c3f25476
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-21 14:01:54 +0900

    プロジェクト内のBlueprintを全部コンパイルするテストコード。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Test/BlueprintCompileTest.h
@@ -0,0 +1,8 @@
+// Copyright 2018 YanaPIIDXer All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AutomationTest.h"
+
+IMPLEMENT_COMPLEX_AUTOMATION_TEST(FBlueprintCompileTest, "AnpanMMO.BlueprintTest", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter);
