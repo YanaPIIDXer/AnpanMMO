27f78155ba39c92557fb5946395d2a4ce0c3f4df
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2018-12-25 18:37:28 +0900

    ターゲットプラットフォームのenum追加。

---
+++ Tool/DLCGenerator/PlatformTarget.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLCGenerator
+{
+
+	/// <summary>
+	/// プラットフォームターゲット
+	/// </summary>
+	public enum EPlatformTarget
+	{
+		/// <summary>
+		/// Windows
+		/// </summary>
+		Windows,
+
+		/// <summary>
+		/// Android
+		/// </summary>
+		Android,
+
+		/// <summary>
+		/// iOS
+		/// </summary>
+		iOS
+	}
+}
