3ae6d66ce65f4c4a7d33524859e7a3051e49c078
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-01-12 20:11:25 +0900

    装備メニュー C++側をある程度実装。

--- Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Equip/EquipMenu.cpp
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Equip/EquipMenu.cpp
@@ -1,9 +1,81 @@
 // Copyright 2018 - 2019 YanaPIIDXer All Rights Reserved.
 
 #include "EquipMenu.h"
+#include "Kismet/GameplayStatics.h"
+#include "Character/Player/GameCharacter.h"
+#include "Master/MasterData.h"
 
 // コンストラクタ
 UEquipMenu::UEquipMenu(const FObjectInitializer &ObjectInitializer)
 	: Super(ObjectInitializer)
+	, BaseMaxHp(0)
+	, BaseStr(0)
+	, BaseDef(0)
+	, BaseInt(0)
+	, BaseMnd(0)
+	, BaseVit(0)
 {
+}
+
+
+// 初期化.
+void UEquipMenu::Init()
+{
+	AGameCharacter *pCharacter = Cast<AGameCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	check(pCharacter != nullptr);
+
+	// 基礎パラメータの設定。
+	const PlayerStatus &Status = pCharacter->GetStatus();
+	BaseMaxHp = Status.GetBaseMaxHp();
+	BaseStr = Status.GetStr();
+	BaseDef = Status.GetDef();
+	BaseInt = Status.GetInt();
+	BaseMnd = Status.GetMnd();
+	BaseVit = Status.GetVit();
+
+	// 現在の装備データ
+	RightEquipData = CreateEquipData(Status.GetRightEquipId());
+	LeftEquipData = CreateEquipData(Status.GetLeftEquipId());
+
+	// 装備リスト
+	TArray<FEquipData> Datas;
+	TArray<uint32> Items = Status.GetItemList().CollectItemList();
+	for (uint32 ItemId : Items)
+	{
+		if (ItemId < 10000) { continue; }
+		for (uint32 i = 0; i < Status.GetItemList().GetCount(ItemId); i++)
+		{
+			FEquipData Data = CreateEquipData(ItemId);
+			Datas.Add(Data);
+		}
+	}
+
+	InitEvent(Datas);
+}
+
+
+// 装備データを生成.
+FEquipData UEquipMenu::CreateEquipData(uint32 EquipId) const
+{
+	FEquipData Data;
+	Data.EquipId = EquipId;
+	const EquipItem *pItem = MasterData::GetInstance().GetEquipMaster().Get(EquipId);
+	if (pItem != nullptr)
+	{
+		Data.Str = pItem->Str;
+		Data.Def = pItem->Def;
+		Data.Int = pItem->Int;
+		Data.Mnd = pItem->Mnd;
+		Data.Vit = pItem->Vit;
+	}
+	else
+	{
+		Data.Str = 0;
+		Data.Def = 0;
+		Data.Int = 0;
+		Data.Mnd = 0;
+		Data.Vit = 0;
+	}
+
+	return Data;
 }
--- Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Equip/EquipMenu.h
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Equip/EquipMenu.h
@@ -6,6 +6,39 @@
 #include "UI/Menu/LayeredMenuWidgetBase.h"
 #include "EquipMenu.generated.h"
 
+// 装備データ
+USTRUCT(BlueprintType, Category = "Equip")
+struct FEquipData
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	// 装備ＩＤ
+	uint32 EquipId;
+
+	// STR
+	UPROPERTY(BlueprintReadOnly)
+	int32 Str;
+
+	// DEF
+	UPROPERTY(BlueprintReadOnly)
+	int32 Def;
+
+	// Int
+	UPROPERTY(BlueprintReadOnly)
+	int32 Int;
+
+	// MND
+	UPROPERTY(BlueprintReadOnly)
+	int32 Mnd;
+
+	// VIT
+	UPROPERTY(BlueprintReadOnly)
+	int32 Vit;
+
+};
+
 /**
  * 装備メニュー
  */
@@ -24,5 +57,81 @@ public:
 	virtual ~UEquipMenu() {}
 
 protected:
+
+	// 初期化.
+	UFUNCTION(BlueprintCallable, Category = "Equip")
+	void Init();
+
+	// 初期化イベント
+	UFUNCTION(BlueprintNativeEvent, Category = "Equip")
+	void InitEvent(const TArray<FEquipData> &Datas);
+	void InitEvent_Implementation(const TArray<FEquipData> &Datas) {}
+
+	// 基礎最大ＨＰ取得.
+	UFUNCTION(BlueprintPure, Category = "Parameter")
+	int32 GetBaseMaxHp() const { return BaseMaxHp; }
+
+	// 基礎STR取得.
+	UFUNCTION(BlueprintPure, Category = "Parameter")
+	int32 GetBaseStr() const { return BaseStr; }
+
+	// 基礎DEF取得.
+	UFUNCTION(BlueprintPure, Category = "Parameter")
+	int32 GetBaseDef() const { return BaseDef; }
+
+	// 基礎INT取得.
+	UFUNCTION(BlueprintPure, Category = "Parameter")
+	int32 GetBaseInt() const { return BaseInt; }
+
+	// 基礎MND取得.
+	UFUNCTION(BlueprintPure, Category = "Parameter")
+	int32 GetBaseMnd() const { return BaseMnd; }
+
+	// 基礎VIT取得.
+	UFUNCTION(BlueprintPure, Category = "Parameter")
+	int32 GetBaseVit() const { return BaseVit; }
+
+	// 右手装備データを取得.
+	UFUNCTION(BlueprintPure, Category = "Equip")
+	const FEquipData &GetRightEquipData() const { return RightEquipData; }
+
+	// 左手装備データを取得.
+	UFUNCTION(BlueprintPure, Category = "Equip")
+	const FEquipData &GetLeftEquipData() const { return LeftEquipData; }
+	
+	// ダミー装備データを生成.
+	// ※装備を外した時等に使用。
+	UFUNCTION(BlueprintPure, Category = "Equip")
+	FEquipData CreateDummyEquipData() const { return CreateEquipData(0); }
+
+private:
+
+	// 基礎最大ＨＰ
+	int32 BaseMaxHp;
+
+	// 基礎STR
+	int32 BaseStr;
+
+	// 基礎DEF
+	int32 BaseDef;
+
+	// 基礎INT
+	int32 BaseInt;
+
+	// 基礎MND
+	int32 BaseMnd;
+
+	// 基礎VIT
+	int32 BaseVit;
+
+	// 右手装備データ
+	FEquipData RightEquipData;
+
+	// 左手装備データ
+	FEquipData LeftEquipData;
+	
+
+	// 装備データを生成.
+	FEquipData CreateEquipData(uint32 EquipId) const;
 
 };
