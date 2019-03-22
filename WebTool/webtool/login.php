4b3aa435541829562e62d73c93c9511a3b91154a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-03-22 14:08:10 +0900

    ログインフォームに何か入力してログインボタンを押下した時、元いたページにリダイレクトするようにした。

--- WebTool/webtool/login.php
+++ WebTool/webtool/login.php
@@ -6,11 +6,37 @@
 <h1>ログインページ</h1>
 <hr />
 <?php
-	print('<form action=' . $_GET['from'] . ' method=\"POST\">');
-	print('ＤＢユーザ名：<input type=text name=\"DBUserName\"><br />');
-	print('ＤＢパスワード：<input type=text name=\"DBPassword\"><br />');
-	print('<input type=submit value=ログイン>');
-	print('</form>');
+	// ログインフォーム
+	print("<form method=\"POST\" action=\"login.php?from=" . $_GET['from'] . "\">\n");
+	print("ＤＢユーザ名：<input type=text name=\"DBUserName\"><br />\n");
+	print("ＤＢパスワード：<input type=text name=\"DBPassword\"><br />\n");
+	print("<input type=submit name=\"LogIn\" value=ログイン><br />\n");
+	print("</form>");
+
+	// ログイン
+	if(isset($_POST['LogIn']))
+	{
+		if(!isset($_POST['DBUserName']) || $_POST['DBUserName'] == "")
+		{
+			print("ユーザ名を入力してください。<br />\n");
+			return;
+		}
+		
+		if(!isset($_POST['DBPassword']) || $_POST['DBPassword'] == "")
+		{
+			print("パスワードを入力してください。<br />\n");
+			return;
+		}
+		
+		// @TODO:ログインチェック。
+		
+		session_start();
+		$_SESSION['DBUserName'] = htmlspecialchars($_POST['DBUserName']);
+		$_SESSION['DBPassword'] = htmlspecialchars($_POST['DBPassword']);
+		
+		// 元いたページにリダイレクト
+		header('Location: ' . $_GET['from']);
+	}
 ?>
 </body>
 </html>
