6ae5d074ba62f3195eb6bf7c055dfe765770c69d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-03-27 05:23:22 +0900

    ・メール送信フォームページ追加。（中身は何も無い） ・トップページに登録者全員にメールを送るためのリンクを配置。

--- WebTool/webtool/index.php
+++ WebTool/webtool/index.php
@@ -12,5 +12,9 @@
 <h1>メインページ</h1>
 <hr />
 <a href="userdata.php">ユーザデータ</a><br />
+<form method="POST" name="BroadcastMail" action="mail.php">
+	<input type="hidden" name="CustomerId" Value=-1>
+	<a href="mail.php" onclick="document.BroadcastMail.aubmit(); return false;">登録者全員にメール送信</a>
+</form>
 </body>
 </html>
