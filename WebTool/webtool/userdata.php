f9078a409d4177156b0ab32f51f47c12c7b308cc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-03-26 11:17:37 +0900

    キャラクタデータへのリンクを作成。

--- WebTool/webtool/userdata.php
+++ WebTool/webtool/userdata.php
@@ -2,33 +2,6 @@
 	require_once('funcs\\session.php');
 	session_start();
 	if(RedirectIfNotLogIn('userdata.php')){ return; }
-	
-	// 装備名を取得.
-	function GetEquipName($Id)
-	{
-		$Conn = MasterConnectionWithSession();
-		if(!$Conn)
-		{
-			print("Master DB Connection Error.<br />\n");
-			return "";
-		}
-		
-		$Stmt = $Conn->prepare("select Name from Equip where ID = :ID;");
-		if(!$Stmt)
-		{
-			print("GetEquipName() Prepare Failed.<br />\n");
-			print($Conn->errorInfo() . "<br />\n");
-			return "";
-		}
-		
-		$Stmt->bindValue(':ID', $Id, PDO::PARAM_INT);
-		$Stmt->execute();
-		$Row = $Stmt->fetch(PDO::FETCH_ASSOC);
-		if(!$Row) { return ""; }
-		
-		$Name = $Row['Name'];
-		return $Name;
-	}
 ?>
 
 <html>
@@ -48,35 +21,27 @@
 	}
 	
 	// ユーザデータテーブルの構築.
-	$Stmt = $Conn->query("select CustomerId, Name, Level, Exp, RightEquip, LeftEquip, IsGM from CharacterData;");
+	$Stmt = $Conn->query("select Id, UserCode, IsBunned from UserData;");
 	print("<table border=1>\n");
 	print("<caption>ユーザデータ</caption>\n");
 	print("<tr style=\"background:#FF00CC\">\n");
 	print("<th>ＩＤ</th>\n");
-	print("<th>キャラクタ名</th>\n");
-	print("<th>レベル</th>\n");
-	print("<th>経験値</th>\n");
-	print("<th>右手装備</th>\n");
-	print("<th>左手装備</th>\n");
-	print("<th>ＧＭフラグ</th>\n");
+	print("<th>ユーザコード</th>\n");
 	print("</tr>\n");
 	while($Row = $Stmt->fetch(PDO::FETCH_ASSOC))
 	{
 		print("<tr>\n");
-		print("<th>" . $Row['CustomerId'] . "</th>\n");
-		print("<th>" . $Row['Name'] . "</th>\n");
-		print("<th>" . $Row['Level'] . "</th>\n");
-		print("<th>" . $Row['Exp'] . "</th>\n");
-		$RightEquip = GetEquipName($Row['RightEquip']);
-		$LeftEquip = GetEquipName($Row['LeftEquip']);
-		print("<th>" . $RightEquip . "</th>\n");
-		print("<th>" . $LeftEquip . "</th>\n");
-		$GMFlag = "";
-		if($Row['IsGM'] == '1')
-		{
-			$GMFlag = "○";
-		}
-		print("<th>" . $GMFlag . "</th>\n");
+		print("<th>" . $Row['Id'] . "</th>\n");
+		print("<th>" . $Row['UserCode'] . "</th>\n");
+
+		// キャラクタページへのリンクを構築.
+		print("<th>\n");
+		print("<form method=\"POST\" name=\"form" . $Row['Id'] . "\" action=\"characterdata.php\">\n");
+		print("	<input type=\"hidden\" name=\"CustomerId\" value=" . $Row['Id'] . ">\n");
+		print("	<a href=\"characterdata.php\" onclick=\"document.form" . $Row['Id'] . ".submit();return false;\">キャラクタデータ閲覧</a>\n");
+		print("</form>\n");
+		
+		print("</th>\n");
 		print("</tr>\n");
 	}
 	print("</table><br />\n");
