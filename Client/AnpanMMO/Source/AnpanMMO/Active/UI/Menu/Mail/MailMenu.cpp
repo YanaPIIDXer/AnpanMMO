d2003d97507be57f927477685651fb0802fe48fe
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-04-03 19:21:00 +0900

    メールリスト画面実装。 ※メールを開く機能はまだ。

---
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Mail/MailMenu.cpp
@@ -0,0 +1,76 @@
+// Copyright 2018 - 2019 YanaPIIDXer All Rights Reserved.
+
+
+#include "MailMenu.h"
+#include "MMOGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "Packet/PacketMailListRequest.h"
+
+// コンストラクタ
+UMailMenu::UMailMenu(const FObjectInitializer &ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+// メールリスト要求送信.
+void UMailMenu::SendMailListRequest()
+{
+	PacketMailListRequest Packet;
+	
+	UMMOGameInstance *pInst = Cast<UMMOGameInstance>(UGameplayStatics::GetGameInstance(this));
+	check(pInst != nullptr);
+
+	pInst->SendPacket(&Packet);
+}
+
+// パケットを受信した。
+void UMailMenu::OnRecvPacket(uint8 ID)
+{
+	switch (ID)
+	{
+		case PacketID::MailList:
+
+			Init();
+			break;
+	}
+}
+
+
+// 初期化.
+void UMailMenu::Init()
+{
+	UMMOGameInstance *pInst = Cast<UMMOGameInstance>(UGameplayStatics::GetGameInstance(this));
+	check(pInst != nullptr);
+
+	const auto &AllMail = pInst->GetMailManager().GetAllMail();
+	TArray<FMailData> List;
+	for (const auto &Mail : AllMail)
+	{
+		FMailData Data;
+		
+		Data.Id = Mail.Id;
+		Data.Subject = UTF8_TO_TCHAR(Mail.Subject.c_str());
+		Data.Body = UTF8_TO_TCHAR(Mail.Body.c_str());
+		switch (Mail.Flag)
+		{
+			case MailData::NotRead:
+
+				Data.Flag = EMailFlag::NotRead;
+				break;
+
+			case MailData::Read:
+
+				Data.Flag = EMailFlag::Read;
+				break;
+
+			case MailData::RecvAttachment:
+
+				Data.Flag = EMailFlag::RecvAttachment;
+				break;
+		}
+
+		List.Add(Data);
+	}
+
+	OnInit(List);
+}
---
+++ Client/AnpanMMO/Source/AnpanMMO/Active/UI/Menu/Mail/MailMenu.h
@@ -0,0 +1,83 @@
+// Copyright 2018 - 2019 YanaPIIDXer All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Menu/LayeredMenuWidgetBase.h"
+#include "MailMenu.generated.h"
+
+// メールフラグ
+UENUM(BlueprintType)
+enum class EMailFlag : uint8
+{
+	// 未読.
+	NotRead,
+
+	// 既読.
+	Read,
+
+	// 添付物受信済み
+	RecvAttachment,
+};
+
+// メールデータ
+USTRUCT(BlueprintType)
+struct FMailData
+{
+
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	// ID
+	uint32 Id;
+
+	// 件名.
+	UPROPERTY(BlueprintReadOnly)
+	FString Subject;
+
+	// 本文.
+	UPROPERTY(BlueprintReadOnly)
+	FString Body;
+
+	// フラグ
+	UPROPERTY(BlueprintReadOnly)
+	EMailFlag Flag;
+
+};
+
+/**
+ * メールメニュー
+ */
+UCLASS()
+class ANPANMMO_API UMailMenu : public ULayeredMenuWidgetBase
+{
+	GENERATED_BODY()
+		
+public:
+
+	// コンストラクタ
+	UMailMenu(const FObjectInitializer &ObjectInitializer);
+
+	// デストラクタ
+	virtual ~UMailMenu() {}
+
+	// メールリスト要求.
+	UFUNCTION(BlueprintCallable, Category = "Mail")
+	void SendMailListRequest();
+
+	// パケットを受信した。
+	virtual void OnRecvPacket(uint8 ID) override;
+
+protected:
+
+	// 初期化.
+	UFUNCTION(BlueprintCallable, Category = "Mail")
+	void Init();
+
+	// 初期化イベント
+	UFUNCTION(BlueprintNativeEvent, Category = "Mail")
+	void OnInit(const TArray<FMailData> &MailList);
+	void OnInit_Implementation(const TArray<FMailData> &MailList) {}
+
+};
