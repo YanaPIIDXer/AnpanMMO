006c63da41252d20e1b498439ea12f7b68a4394c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-06-29 00:42:26 +0900

    ドキュメントの更新。

--- docs/SourceDocument/Client/Document/search/searchdata.js
+++ docs/SourceDocument/Client/Document/search/searchdata.js
@@ -1,36 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghijklmnopqrstuvwxyz~",
-  1: "abcefghimnpqstuvw",
-  2: "abcefghilmnopqstuvwy",
-  3: "abcdefghiklmnopqrstuvw~",
-  4: "abcdefghijklmnopqrstuvwxyz",
-  5: "su",
-  6: "acemnprst",
-  7: "abcdefghiklmnopqrstu"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "typedefs",
-  6: "enums",
-  7: "enumvalues"
 };
 
 var indexSectionLabels =
 {
-  0: "全て",
-  1: "クラス",
-  2: "ファイル",
-  3: "関数",
-  4: "変数",
-  5: "型定義",
-  6: "列挙型",
-  7: "列挙値"
 };
 
--- docs/SourceDocument/Server/GameServer/Document/search/searchdata.js
+++ docs/SourceDocument/Server/GameServer/Document/search/searchdata.js
@@ -1,42 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghijklmnopqrstuvwxyz~",
-  1: "abcdefghilmnpqrstuvw",
-  2: "b",
-  3: "abcdefghilmnpqrstuvw",
-  4: "abcdefghijlmnopqrstuvw~",
-  5: "abcdefghijklmnopqrstuvwxyz",
-  6: "acprstu",
-  7: "acemnprst",
-  8: "abcdefghiklmnopqrstuw",
-  9: "_"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "typedefs",
-  7: "enums",
-  8: "enumvalues",
-  9: "defines"
 };
 
 var indexSectionLabels =
 {
-  0: "全て",
-  1: "クラス",
-  2: "名前空間",
-  3: "ファイル",
-  4: "関数",
-  5: "変数",
-  6: "型定義",
-  7: "列挙型",
-  8: "列挙値",
-  9: "マクロ定義"
 };
 
